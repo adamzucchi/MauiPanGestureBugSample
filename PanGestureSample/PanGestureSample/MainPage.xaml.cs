2557bef4f9c41e5d02b4bd2f047ef80408753580
Adam Zucchi <adam@adamzucchi.com>
2023-09-01 07:49:21 -0500

    - initial commit

---
+++ PanGestureSample/PanGestureSample/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace PanGestureSample;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    void PanGestureRecognizer_PanUpdated(System.Object sender, Microsoft.Maui.Controls.PanUpdatedEventArgs e)
+    {
+		Debug.WriteLine($"StatusType: {e.StatusType} TotalX: {e.TotalX} TotalY:{e.TotalY}");
+    }
+}
+
+
